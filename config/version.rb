e27e31ddefa69d20ea1803d66bf3f3e20a6c3867
BinaryBabel OSS <oss@binarybabel.org>
2016-12-27 02:27:34 -0800

    Fix versioneer in production.

--- config/version.rb
+++ config/version.rb
@@ -2,7 +2,11 @@ require 'versioneer'
 
 module Latestver
   RELEASE = '1.5'
-  VERSION = Versioneer::Git.new(__FILE__,
-                                :prereleases => %w(alpha beta),
-                                :starting_release => self::RELEASE).to_s
+  begin
+    VERSION = Versioneer::Git.new(__FILE__,
+                                  :prereleases => %w(alpha beta),
+                                  :starting_release => self::RELEASE).to_s
+  rescue Versioneer::InvalidRepoError
+    VERSION = self::RELEASE
+  end
 end
