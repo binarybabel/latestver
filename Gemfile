b82f00f5e17ce21ead3e196a5738630a4fbf6665
BinaryBabel OSS <oss@binarybabel.org>
2017-01-01 04:17:18 -0800

    Update to Versioneer 0.3

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'rufus-scheduler'
 gem 'webhook'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
-gem 'versioneer', '~> 0.2'
+gem 'versioneer', '~> 0.3'
 
 # Version checking helpers
 gem 'gems'
--- config/version.rb
+++ config/version.rb
@@ -3,9 +3,7 @@ require 'versioneer'
 module Latestver
   RELEASE = '1.6'
   begin
-    VERSION = Versioneer::Git.new(__FILE__,
-                                  :prereleases => %w(alpha beta),
-                                  :starting_release => self::RELEASE).to_s
+    VERSION = Versioneer::Config.new(File.expand_path('../../', __FILE__)).to_s
   rescue Versioneer::InvalidRepoError
     VERSION = self::RELEASE
   end
