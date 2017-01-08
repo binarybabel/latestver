8a5edff428b9c3def882cea6caa439797ac18e2e
BinaryBabel OSS <oss@binarybabel.org>
2017-01-08 02:13:57 -0800

    Restyle API examples.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def instance_groups
     Group.order(:name).all.map { |g| g.name }
   end
+
+  def code_control(css_class, input)
+    (%%<code class="#{css_class} form-control">% +
+        input.gsub(/<([^>]+)>/, '<span class="token">&lt;\\1&gt;</span>') +
+        '</code>').html_safe
+  end
 end
