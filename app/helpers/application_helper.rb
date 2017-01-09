8e97868d28659f205672c4fc8658577475f4ae76
BinaryBabel OSS <oss@binarybabel.org>
2017-01-09 01:35:44 -0800

    Add HTML view for catalog entries.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 module ApplicationHelper
+  def body_class
+    c = params[:controller].to_s.split('/').last
+    a = params[:action]
+    "#{c} #{c}-#{a}"
+  end
+
   def last_catalog_refresh
     time = CatalogEntry.order('refreshed_at DESC').first.try(:refreshed_at)
     time && time.to_s || 'Never'
@@ -8,9 +14,10 @@ module ApplicationHelper
     Group.order(:name).all.map { |g| g.name }
   end
 
-  def code_control(css_class, input)
-    (%%<code class="#{css_class} form-control">% +
-        input.gsub(/<([^>]+)>/, '<span class="token">&lt;\\1&gt;</span>') +
-        '</code>').html_safe
+  def input_sample(input, opts=nil)
+    opts = {theme: '', icon: 'terminal', label: ''}.merge(opts.to_h)
+    (%%<div class="input-sample #{opts[:theme]} form-control"><i class="fa fa-#{opts[:icon]}"> #{opts[:label]}</i><code>% +
+        input.gsub(/<([^>]+)>/, '<span class="token">\\1</span>') +
+        '</code></div>').html_safe
   end
 end
